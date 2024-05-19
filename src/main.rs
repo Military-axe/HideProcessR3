bfa018255948cb97a4960746d9ec7313055a8a20
mi1itray.axe <mi1itray.axe@gmail.com>
2024-05-19 23:07:56 +0800

    refactor🎨: 修改命令行参数结构

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,52 @@
 mod process;
 
+
 use crate::process::{FakeProcess, ObjProcess, Process};
-use clap::{command, Parser};
+use clap::{command, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long)]
-    obj: u32,
-
-    #[arg(short, long)]
-    fake: u32,
+    #[command(subcommand)]
+    copy_str: Option<CopyStr>
 }
 
-fn main() {
-    let args: Cli = Cli::parse();
-    let obj = ObjProcess::from(args.obj);
-    let mut fake = FakeProcess::from(args.fake);
-    println!("[+] obj process: {:?}", obj);
-    println!("[+] fake process: {:?}", fake);
+#[derive(Subcommand)]
+enum CopyStr {
+    // copy some strings of process `obj` to process `fake`.
+    Add{
+        #[arg(short, long)]
+        obj: u32,
 
-    let command_line = obj
+        #[arg(short, long)]
+        fake: u32,
+    }
+}
+
+fn copy_str_2_process(obj: u32, fake: u32) {
+    let obj_process = ObjProcess::from(obj);
+    let mut fake_process = FakeProcess::from(fake);
+    let command_line = obj_process
         .get_command_line_2_vec()
         .expect("[! Hide Process R3] Get Command Line failed.");
-    let image_name = obj
+    let image_name = obj_process
         .get_image_name_2_vec()
         .expect("[! Hide Process R3] Get Image Name failed.");
     println!("{:#?}", Process::pwstr_to_string(&command_line));
     println!("{:#?}", Process::pwstr_to_string(&image_name));
 
-    fake.set_command_line(command_line)
+    fake_process.set_command_line(command_line)
         .expect("[! Hide Process R3] Set Command Line failed.");
-    fake.set_image_name(image_name)
-        .expect("[! Hide Process R3] Set Image Name failed.");
+    fake_process.set_image_name(image_name)
+        .expect("[! Hide Process R3] Set Image Name failed.")
+}
+
+fn main() {
+    let args: Cli = Cli::parse();
+    match &args.copy_str {
+        Some(CopyStr::Add { obj, fake }) => {
+            copy_str_2_process(*obj, *fake)
+        },
+        _ => {}
+    }
 }
